d6565167f44d505af87fc825b186410f85dd10a5
lucas.dalan <lucasdalan09@gmail.com>
2025-04-30 16:02:00 -0300

    feat: added cors

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ import teamRouter from "./routes/team.routes";
 import taskRouter from "./routes/task.routes";
 import { HonoApp } from "./lib/utils/types";
 import { databaseMiddleware } from "./lib/middlewares/database";
+import { cors } from "hono/cors";
 
 const app: HonoApp = new Hono();
 
 app.use("*", databaseMiddleware);
+app.use("*", cors());
 
 app.route("/users", userRouter);
 app.route("/auth", authRouter);
